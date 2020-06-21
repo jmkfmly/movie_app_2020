e666a05540a9c8f8789b6d6f14fec9a27a6d85dc
Song manho <jmkfmly@naver.com>
2020-06-21 22:52:47 +0900

    Updates

---
+++ precache-manifest.b9f774d3ed4f30f4f89fbfeb9df032d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d557e97dd5cb007af9b942e84bbbcb",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "ded0ea8075c8c97060ce",
+    "url": "/movie_app_2020/static/css/main.fc4957d1.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded0ea8075c8c97060ce",
+    "url": "/movie_app_2020/static/js/main.915c6b58.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
