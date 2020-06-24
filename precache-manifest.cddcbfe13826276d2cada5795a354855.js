40ec40d40f382452bc4f8169197d997e33971040
Song manho <jmkfmly@naver.com>
2020-06-24 14:38:46 +0900

    Updates

---
+++ precache-manifest.cddcbfe13826276d2cada5795a354855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee595afb820ca3e56855e57f3d24be5",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "e5577b82e658536df454",
+    "url": "/movie_app_2020/static/css/main.fa2c2541.chunk.css"
+  },
+  {
+    "revision": "1d60617635490a4eeafa",
+    "url": "/movie_app_2020/static/js/2.0bf139cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.0bf139cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5577b82e658536df454",
+    "url": "/movie_app_2020/static/js/main.9620474c.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_2020/precache-manifest.609b36b1f0aac7bada01f51134fc1fb4.js"
+  "/movie_app_2020/precache-manifest.cddcbfe13826276d2cada5795a354855.js"
 );
 
 self.addEventListener('message', (event) => {
